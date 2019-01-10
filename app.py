06c7552cff88cae9f11f6eba714634ec127f5f11
Christina <Christinepeninsula@hotmail.com>
2019-01-10 16:31:52 -0500

    changed the class names and css, restarted app.py completely

--- app.py
+++ app.py
@@ -1,57 +1,28 @@
+
 from flask import Flask
 from flask import request
 
 app = Flask(__name__)
 
-@app.route("/vertical_timeline")
+@app.route("/")
+def index():
 
+    input = request.args.get('search-term')
+    print(input)
 
-def switch_styles():
-
-    input = request.args.get("vertical_timeline ")
+    # open file for reading, 'r'
+    # file is saved to variable
     index_file = open('index.html', 'r')
-
-
-    style_switch=index_file.read()
-
-
+    # read contents of the file
+    my_html = index_file.read()
 
     if input:
-
-
-       style_switch= style_switch.replace(" <link class="style1" rel="stylesheet"  href="./static/styles.css" type="text/css">",
-       < link class ="style2" rel="alternate stylesheet"  href="./static/styles2.css" type="text/css" >" )
-
+        # add use input back into the html
+        my_html = my_html.replace("{{search-term-value}}",input)
     else:
-        style_switch= style_switch.replace("<link class="style1" rel="stylesheet"  href="./static/styles.css" type="text/css">", " ")
+        my_html = my_html.replace("{{search-term-value}}","")
 
-
-
-
-
-
+    # close the file out when you're done
     index_file.close()
-    return switch_styles
 
-
-# def index():
-#
-#     input = request.args.get('refresh')
-#
-#
-#     # open file for reading, 'r'
-#     # file is saved to variable
-#     index_file = open('index.html', 'r')
-#     # read contents of the file
-#     my_html = index_file.read()
-#
-#     if input:
-#
-#
-#         # add use input back into the html
-#         my_html = my_html.replace("style1", )
-#
-#     # close the file out when you're done
-#     index_file.close()
-#
-#     return my_html
+    return my_html
